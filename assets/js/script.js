a53591441677b248399ad92efb418e7c7582f75f
Máximo cavero <131488344+m4xcav@users.noreply.github.com>
2023-07-20 09:37:40 -0400

    corrigiendo 3_pintar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
+//scrip a modificar
+
+// function pintar(){
+// ele = document.getElementById("ele1")
+// ele.style.backgroundColor = "yellow"
+// }
+// ele = document.getElementById("ele1")
+// ele.addEventListener("click", pintar);
+
+//modificacion del script anterior
 function pintar(elem, color){
     elem.style.backgroundColor = color;
     }
     const ele = document.getElementById("ele1");
-    document.addEventListener("DOMContentLoaded", pintar(ele, "green"));
-    ele.addEventListener("click", ()=>pintar(ele, "yellow"));
+    let color
+    document.addEventListener("DOMContentLoaded", pintar(ele, color = "green"));
+    ele.addEventListener("click", ()=>pintar(ele, color = "yellow"));
